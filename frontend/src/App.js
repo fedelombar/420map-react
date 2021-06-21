dc02e981eae2735fd66de1ba48518b2e8419b281
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-06-21 17:56:52 -0300

    map styles & fixs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,63 @@
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+
 function App() {
-  return <div className="App">hello</div>;
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 40,
+    longitude: -93,
+    zoom: 4,
+  });
+
+  return (
+    <div className="App">
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/frederickmongo/ckq7251ye90sj17s25qbcle3a"
+      >
+        <Marker
+          latitude={40.7127}
+          longitude={-74.0134}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <Room style={{ fontSize: viewport.zoom * 7, color: "slateblue" }} />
+        </Marker>
+        <Popup
+          latitude={40.7127}
+          longitude={-74.0134}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left"
+        >
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">Memorial Center</h4>
+            <label>Review</label>
+            <p>Beautiful Place. I like it.</p>
+            <label>Rating</label>
+            <div className="stars">
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+            </div>
+
+            <label>Information</label>
+            <span className="username">
+              Created by <b>safak</b>
+            </span>
+            <span className="date"> 1 hour ago</span>
+          </div>
+        </Popup>
+      </ReactMapGL>
+    </div>
+  );
 }
 
 export default App;
